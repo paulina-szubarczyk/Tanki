c2cd911cd4ec2921352187ec152a3f7e672e45da
kjlis <krzysiek.lisu@gmail.com>
2014-05-31 23:32:58 +0200

    Added ClientConnection implementation

--- src/app/client_lib/ClientConnection.cpp
+++ src/app/client_lib/ClientConnection.cpp
@@ -11,4 +11,14 @@ void ClientConnection::setGridSize(int n){
 	GameWindow::getInstance().getManager().setGridSize(n);
 }
 
+void ClientConnection::setShipsConfig(std::map<int,int>& config){
+	GameWindow::getInstance().getManager().setShipsQuantity(config);
+}
 
+void ClientConnection::setHitReply(int x, int y, bool result){
+	GameWindow::getInstance().setHitResult(x,y,result);
+}
+
+void ClientConnection::setTurn(bool turn){
+	GameWindow::getInstance().setPlayerTurn(turn);
+}
