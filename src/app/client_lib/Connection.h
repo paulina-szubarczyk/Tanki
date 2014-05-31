0e795b3bdd3ada0744457aac46bd4b32de84bc43
kjlis <krzysiek.lisu@gmail.com>
2014-05-31 07:34:50 +0200

    Added Connection interface

---
+++ src/app/client_lib/Connection.h
@@ -0,0 +1,41 @@
+/*
+ * Connection.h
+ *
+ *  Created on: May 31, 2014
+ *      Author: klis
+ */
+
+#ifndef CONNECTION_H_
+#define CONNECTION_H_
+
+
+class Connection{
+public:
+	int getGridSize(); // returns grid size
+	/*
+	 * Returns map:
+	 * first is ship size
+	 * second is quantity of ships for each ship size
+	 */
+	std::map<int,int> getShipsConfig();
+	void connect(std::string serverIP); //connect to server
+	/*
+	 * Send ship to server:
+	 * ship is vector of <int,int> pairs where:
+	 * first is X coordinate
+	 * second is Y coordinate
+	 */
+	void sendShip(std::vector<std::pair<int,int> > ship);
+	/*
+	 * Send hit to server
+	 * Hit is a <int,int> pair where
+	 * first is X coordinate
+	 * second is Y coordinate
+	 */
+	void sendHit(std::pair<int,int>);
+
+	std::pair<int,int> getHit(); //get hit from server
+
+};
+
+#endif /* CONNECTION_H_ */
